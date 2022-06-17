52a0e918506073ada4bbbbafb3d952b2003776c3
garretthilberling <ghilberling@knights.ucf.edu>
2022-06-16 21:49:41 -0400

    learned how to println (think console.log) and how variables are defined strictly as types (think TypeScript)

---
+++ src/main/java/com/technews/technewsjavaapi/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.technews.technewsjavaapi;
+
+public class HelloWorld {
+    // main resembles process.argv in Node.js (passes in arguments to the program)
+    public static void main(String[] args) {
+        // same as console logging in js
+        System.out.println("Hello, world!"); // println() belongs to System.out object just as log() belongs to console in js
+    }
+}
---
+++ src/main/java/com/technews/technewsjavaapi/VariableTypes.java
@@ -0,0 +1,11 @@
+package com.technews.technewsjavaapi;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        String name = "Cleopatra"; // indicates a string variable
+        int age = 2016 + 69; // indicates any number
+        double price = 1999.99; // indicates number with decimal values
+        boolean truth = true; // indicates true/false
+        boolean falseness = !truth;
+    }
+}
